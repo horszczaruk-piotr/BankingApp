3d5276911a7344215e2cb53c8dba888ee71a9c24
horszczaruk-piotr <horszczaruk.piotr@gmail.com>
2023-04-02 22:02:24 +0200

    transfer Money

--- src/main/java/com/example/bankingapp/model/Account.java
+++ src/main/java/com/example/bankingapp/model/Account.java
@@ -1,2 +1,32 @@
-package com.example.bankingapp.model;public class Account {
+package com.example.bankingapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Account {
+    @Id
+    private String accountNumber;
+    @Transient
+    private String accountInput;
+    private String pin;
+    double balance;
+
+
+    public String getLogin(){
+        return accountNumber.substring(accountNumber.length() - 5);
+    }
+
+    @Override
+    public String toString() {
+        return "Account details: " +
+                "Your account number='" + accountNumber + '\'' +
+                ", balance=" + balance +
+                '.';
+    }
 }
--- src/main/java/com/example/bankingapp/service/UserDashboardService.java
+++ src/main/java/com/example/bankingapp/service/UserDashboardService.java
@@ -1,2 +1,57 @@
-package com.example.bankingapp.service;public class UserDashboardService {
+package com.example.bankingapp.service;
+
+import com.example.bankingapp.model.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Scanner;
+@Service
+@RequiredArgsConstructor
+public class UserDashboardService {
+    private final AccountService accountService;
+
+    public void showDashboard(Account loggedAccount){
+
+        boolean isLogged = true;
+        while(isLogged){
+            System.out.println(loggedAccount.toString());
+            int selectedOption = chooseDashboardOption();
+
+            switch (selectedOption){
+                case 1:
+                    accountService.addMoney(loggedAccount);
+                    break;
+                case 2:
+                    accountService.withdrawMoney(loggedAccount);
+                    break;
+               case 3:
+                   accountService.initializeTransferMoney(loggedAccount);
+                case 0:
+                    isLogged = false;
+                    break;
+            }
+        }
+
+
+    }
+
+    public int chooseDashboardOption(){
+        boolean isDashboard = true;
+        while(isDashboard){
+            System.out.println("Press 1 to add money.");
+            System.out.println("Press 2 to withdraw money.");
+            System.out.println("Press 3 to transfer money.");
+            System.out.println("Press 0 to log out.");
+            try {
+                Scanner scanner = new Scanner(System.in);
+                int selectedOption = scanner.nextInt();
+                isDashboard = false;
+                return  selectedOption;
+            } catch (Exception exception) {
+                System.out.println("You have type the wrong format. Please use correct numbers. ");
+            }
+
+        }
+        return 0;
+    }
 }
